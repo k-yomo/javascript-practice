102deb52bc349363775a36f6eab33ea77b6aca56
Kanji Yomoda <kanji.yy@gmail.com>
2017-07-08 21:45:18 -0700

    Wrote symple arrays and built-in function used for arrays

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ yearsUntilRetirement('Mary', 1948);
 
 
 // statements and expressions
-
+/*
 //function statement
 function someFun(par) {
   //code
@@ -197,6 +197,67 @@ function someFun(par) {
 var someFun = function(par) {
   //code
 }
+*/
+
+
+// arrays
+
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+console.log(john);
+
+if(john.indexOf('teacher') === -1){
+  console.log('John is not a teacher.')
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
