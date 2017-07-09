e69324fc7885d85a38ee0ed9e561315106c20d2d
Kanji Yomoda <kanji.yy@gmail.com>
2017-07-09 16:10:53 -0700

    Write a method in a object using 'this'

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ if(john.indexOf('teacher') === -1){
 
 
 // Objects
- 
+ /*
  var john = {
   name: 'John',
   lastName: 'Smith',
@@ -253,6 +253,60 @@ jane['job'] = 'retired';
 jane['isMarried'] = true;
 
 console.log(jane);
+*/
+
+// Objects and methods
+// v1.0
+/*
+ var john = {
+  name: 'John',
+  lastName: 'Smith',
+  yearOfBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  calculateAge: function(){
+    return 2017 - this.yearOfBirth;
+  }
+ };
+
+console.log(john.calculateAge());
+
+var age = john.calculateAge();
+john.age = age;
+console.log(john);
+*/
+// v2.0
+
+var john = {
+  name: 'John',
+  lastName: 'Smith',
+  yearOfBirth: 1990,
+  job: 'teacher',
+  isMarried: false,
+  family: ['Jane', 'Mark', 'Bob'],
+  calculateAge: function(){
+    this.age = 2016 - this.yearOfBirth;
+  }
+ };
+
+ john.calculateAge();
+ console.log(john);
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
