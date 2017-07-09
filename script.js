9cadc8cd51217e1fbcdb02d3b447fd459ddd19a5
Kanji Yomoda <kanji.yy@gmail.com>
2017-07-09 16:29:25 -0700

    Wrote for loops and while loops

--- script.js
+++ script.js
@@ -278,6 +278,7 @@ console.log(john);
 */
 // v2.0
 
+/*
 var john = {
   name: 'John',
   lastName: 'Smith',
@@ -292,27 +293,41 @@ var john = {
 
  john.calculateAge();
  console.log(john);
-
- 
-
+*/
 
 
+//Loops and iteration
 
 
+for(var i = 0; i < 10; i++){
+  console.log(i);
+}
 
 
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+for(var i = 0; i < names.length; i++){
+  console.log(names[i]);
+}
 
 
+for(var i = name.length - 1; i >= 0; i--){
+  console.log(names[i]);
+}
+
+var i = 0;
+while(i < names.length){
+  console.log(names[i]);
+  i++;
+}
 
 
-
-
-
-
-
-
-
-
+for(var i = 1; i <= 5; i++){
+    if(i === 3){
+    continue;
+  }
+  console.log(i);
+}
 
 
 
