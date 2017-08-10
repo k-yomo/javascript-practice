721582755447cb053ba257e5a5318a3f9cc034bc
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-10 16:56:55 -0700

    Add another dice to the game

--- 4_DOM_pig_game/app.js
+++ 4_DOM_pig_game/app.js
@@ -2,29 +2,34 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a  as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundscores, activePlayer, gamePlaying, lastDice;
+var scores, roundscores, winningScore, activePlayer, gamePlaying, lastDice1, lastDice2;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gamePlaying) {
-    var dice = Math.ceil(Math.random() * 6);
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = "block";
-    diceDOM.src = "dice-" + dice + ".png";
-    if (lastDice === 6 && dice === 6) {
+    var dice1 = Math.ceil(Math.random() * 6);
+    var dice2 = Math.ceil(Math.random() * 6);
+    var diceDOM1 = document.querySelector('.dice1');
+    var diceDOM2 = document.querySelector('.dice2');
+    diceDOM1.style.display = "block";
+    diceDOM1.src = "dice-" + dice1 + ".png";
+    diceDOM2.style.display = "block";
+    diceDOM2.src = "dice-" + dice2 + ".png";
+    if ((lastDice1 === 6 && dice1 === 6) || (lastDice2 === 6 && dice2 === 6)) {
       scores[activePlayer] = 0;
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
       nextPlayer();
-    }else if (dice !== 1) {
-      roundScore += dice;
-      lastDice = dice;
+    } else if (dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice1 + dice2;
+      lastDice1 = dice1;
+      lastDice2 = dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       nextPlayer();
@@ -38,9 +43,10 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
   }
 
-  if (scores[activePlayer] >= 100) {
+  if (scores[activePlayer] >= winningScore) {
     document.getElementById('name-' + activePlayer).textContent = "Winner";
-    document.querySelector('.dice').style.display = "none";
+    document.querySelector('.dice1').style.display = "none";
+    document.querySelector('.dice2').style.display = "none";
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     gamePlaying = false;
@@ -52,11 +58,14 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
+  lastDice1 = 0;
+  lastDice2 = 0;
   document.getElementById('current-0').textContent = "0";
   document.getElementById('current-1').textContent = "0";
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
-  document.querySelector('.dice').style.display = "none";
+  document.querySelector('.dice1').style.display = "none";
+  document.querySelector('.dice2').style.display = "none";
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -66,8 +75,12 @@ function init() {
   roundScore = 0;
   activePlayer = 0;
   gamePlaying = true;
+  lastDice1 = 0;
+  lastDice2 = 0;
+  winningScore = document.getElementById('winning-score').value;
 
-  document.querySelector('.dice').style.display = "none";
+  document.querySelector('.dice1').style.display = "none";
+    document.querySelector('.dice2').style.display = "none";
 
   for(var i = 0; i < 2; i++) {
     document.getElementById('score-' + i).textContent = "0";
