fd64e80b4da685a517ced4518f9d4246e292884c
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-10 00:31:50 -0700

    Add event listener to roll dice and display the result

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,3 +8,36 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundscores, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = "none";
+
+for(var i = 0; i < 2; i++) {
+  document.getElementById('score-' + i).textContent = "0";
+  document.getElementById('current-' + i).textContent = "0";
+}
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  var dice = Math.ceil(Math.random() * 6);
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = "0";
+    document.getElementById('current-1').textContent = "0";
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = "none";
+  }
+});
