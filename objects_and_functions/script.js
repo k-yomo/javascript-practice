6d995d9cbe741ce72579f87d89b79461c9bcc554
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-11 11:26:08 -0700

    Create a call back function

--- objects_and_functions/script.js
+++ objects_and_functions/script.js
@@ -28,23 +28,94 @@
 // console.log(mark.lastName);
 
 
-// Object.create
-var personProto = {
-      calculateAge: function() {
-        console.log(2017 - this.yearOfBirth);
-      }
-};
+// // Object.create
+// var personProto = {
+//       calculateAge: function() {
+//         console.log(2017 - this.yearOfBirth);
+//       }
+// };
+//
+// var john = Object.create(personProto);
+// john.name = "John";
+// john.yearOfBirth = 1990;
+// john.job = "teacher";
+//
+// var jane = Object.create(personProto,
+//   {
+//   name: { value: "Jane" },
+//   yearOfBirth: { value: 1969 },
+//   job: { value: "designer" }
+// });
+//
+// console.log(john.calculateAge());
 
-var john = Object.create(personProto);
-john.name = "John";
-john.yearOfBirth = 1990;
-john.job = "teacher";
 
-var jane = Object.create(personProto,
-  {
-  name: { value: "Jane" },
-  yearOfBirth: { value: 1969 },
-  job: { value: "designer" }
-});
+// // Premitives vs objects
+// // Premitives
+// var a = 23;
+// var b = a;
+// a = 46;
+// console.log(a);
+// console.log(b);
+//
+// // Pbjects
+// var obj1 = {
+//     name: "John",
+//     age: 26
+// };
+// var obj2 = obj1;
+// obj1.age = 30;
+// console.log(obj1);
+// console.log(obj2);
+//
+// // function
+// var age = 27;
+// var obj = {
+//   name: "John",
+//   city: "Lisbon"
+// };
+//
+// function change(a, b) {
+//   a = 30;
+//   b.city  = "San Francisco";
+// };
+// change(age, obj);
+//
+// console.log(age);
+// console.log(obj.city);
 
-console.log(john.calculateAge());
+
+// Passing functions as arguments
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return  arrRes;
+}
+
+function calculateAge(el) {
+  return 2017 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+  } else {
+      return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var isFullAge = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(isFullAge);
+console.log(ages);
+console.log(rates);
