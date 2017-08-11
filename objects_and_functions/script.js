feddc473a90ea126300203f340f2fed88ccfbd11
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-11 10:52:35 -0700

    Create a prototype using object create

--- objects_and_functions/script.js
+++ objects_and_functions/script.js
@@ -1,29 +1,50 @@
-// Fuction constructor
+// // Fuction constructor
+// var john = {
+//   name: "John",
+//   yearOfBirth: 1990,
+//   job: "teacher"
+// };
+//
+// var Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// }
+// Person.prototype.calculateAge = function() {
+//   console.log(2017 - this.yearOfBirth);
+// };
+//
+// Person.prototype.lastName = "Smith"
+// var john = new Person("John", 1990, "teacher");
+// var jane = new Person("Jane", 1969, "designer");
+// var mark = new Person("Mark", 1948, "retired");
+//
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+//
+// console.log(john.lastName);
+// console.log(jane.lastName);
+// console.log(mark.lastName);
 
-var john = {
-  name: "John",
-  yearOfBirth: 1990,
-  job: "teacher"
+
+// Object.create
+var personProto = {
+      calculateAge: function() {
+        console.log(2017 - this.yearOfBirth);
+      }
 };
 
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-}
-Person.prototype.calculateAge = function() {
-  console.log(2017 - this.yearOfBirth);
-};
+var john = Object.create(personProto);
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = "teacher";
 
-Person.prototype.lastName = "Smith"
-var john = new Person("John", 1990, "teacher");
-var jane = new Person("Jane", 1969, "designer");
-var mark = new Person("Mark", 1948, "retired");
+var jane = Object.create(personProto,
+  {
+  name: { value: "Jane" },
+  yearOfBirth: { value: 1969 },
+  job: { value: "designer" }
+});
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+console.log(john.calculateAge());
