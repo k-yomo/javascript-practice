0c4a3453389a4c4b514d233c9fd42b7442fdd7e9
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-12 15:12:40 -0700

    Add initialize function

--- 6_budgety/app.js
+++ 6_budgety/app.js
@@ -5,18 +5,49 @@ var budgetController = (function() {
 
 var uiController = (function() {
 
+  var domStrings = {
+    inputType: '.add__type',
+    inputDiscription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
+  }
+
+  return {
+    getInput: function() {
+      return  {
+        type: document.querySelector(domStrings.inputType).value,
+        description: document.querySelector(domStrings.inputDiscription).value,
+        value: document.querySelector(domStrings.inputValue).value
+      };
+    },
+    getDomStrings: function() {
+      return domStrings;
+    }
+  };
 })();
 
 
 var controller = (function(budgetCtrl, uiCtrl) {
-  var ctrlAddItem = function() {
-    console.log('It works');
-  };
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-  document.addEventListener('keypress', function(e) {
-    if (e.keyCode === 13 || e.which === 13) {
-      ctrlAddItem();
-    }
-  });
+  var dom = uiCtrl.getDomStrings();
+  var setUpEventListeners = function() {
+    document.querySelector(dom.inputButton).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(e) {
+      if (e.keyCode === 13 || e.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  }
 
+  var ctrlAddItem = function() {
+    var input = uiCtrl.getInput();
+    console.log(input);
+  };
+
+  return {
+    init: function() {
+      setUpEventListeners();
+    }
+  }
 })(budgetController, uiController);
+
+controller.init();
