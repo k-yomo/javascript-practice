6e635ca5d299d85a6bf8ecdc8c8e267a459c4155
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-13 12:49:52 -0700

    Implement IIFE using es6

--- 7_ecma_script2015/script.js
+++ 7_ecma_script2015/script.js
@@ -11,32 +11,44 @@
 // name6 = 'Jane Miller';
 // console.log(name6);
 
-// ES5
-function driversLicense5(passedTest) {
-  if (passedTest) {
-    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
-  }
-  var firstName = 'John';
-  var yearOfBirth = 1990;
+// // ES5
+// function driversLicense5(passedTest) {
+//   if (passedTest) {
+//     console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+//   }
+//   var firstName = 'John';
+//   var yearOfBirth = 1990;
+// }
+//
+// // ES6
+// driversLicense5(true);
+//
+// function driversLicense(passedTest) {
+//   let firstName;
+//   const yearOfBirth = 1990;
+//   if (passedTest) {
+//     firstName = 'John';
+//   }
+//   console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+// }
+//
+// driversLicense(true);
+//
+// let i = 23;
+//
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+
+// Blocks and IIFEs
+
+{
+  const a = 1;
+  let b =2;
+  var c = 3;
 }
 
-// ES6
-driversLicense5(true);
-
-function driversLicense(passedTest) {
-  let firstName;
-  const yearOfBirth = 1990;
-  if (passedTest) {
-    firstName = 'John';
-  }
-  console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
-}
-
-driversLicense(true);
-
-let i = 23;
-
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-console.log(i);
+console.log(a + b);
+// console.log(c);
