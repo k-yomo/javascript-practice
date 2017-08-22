6e85356118845ceeec15593c8979d787f5825e1f
Kanji Yomoda <kanji.yy@gmail.com>
2017-08-22 14:29:44 -0700

    Create a constructor using default parameter

--- 7_ecma_script2015/script.js
+++ 7_ecma_script2015/script.js
@@ -252,25 +252,48 @@
 // Array.from(all).forEach(el => el.style.color = 'purple');
 
 
-// Lest parameters
+// // Lest parameters
+// // ES5
+// function isFullAge5() {
+//   var argsArr = Array.prototype.slice.call(arguments);
+//   argsArr.forEach(function(el) {
+//     console.log((2017 - el) >= 18);
+//   })
+// }
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+//
+// // ES6
+// function isFullAge6(...years) {
+//   years.forEach(el => console.log((2017 - el) >= 18));
+// }
+//
+// isFullAge6(1990, 1999, 1965, 2016, 1987);
+//
+// function isFullAge6(limit, ...years) {
+//   years.forEach(el => console.log((2018 - el) >= limit));
+// }
+//
+// isFullAge6(16,1990, 1999, 1965, 2016, 1987);
+
+
+// Default parameters
 // ES5
-function isFullAge5() {
-  var argsArr = Array.prototype.slice.call(arguments);
-  argsArr.forEach(function(el) {
-    console.log((2017 - el) >= 18);
-  })
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+  lastName === undefined ? lastName = 'Smith' : lastName;
+  nationality === undefined ? nationality = 'american' : nationality;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
 }
-isFullAge5(1990, 1999, 1965, 2016, 1987);
 
 // ES6
-function isFullAge6(...years) {
-  years.forEach(el => console.log((2017 - el) >= 18));
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
 }
 
-isFullAge6(1990, 1999, 1965, 2016, 1987);
-
-function isFullAge6(limit, ...years) {
-  years.forEach(el => console.log((2018 - el) >= limit));
-}
-
-isFullAge6(16,1990, 1999, 1965, 2016, 1987);
+var  john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
